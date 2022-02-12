2b7c1b43653e740db0175807ee2ba1490c80008b
azarnuzy <azarnuzy@gmail.com>
2022-02-12 13:54:34 +0700

    make get notes and get node by id function

--- Learning/notes-app-back-end/src/services/postgres/NotesService.js
+++ Learning/notes-app-back-end/src/services/postgres/NotesService.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
+const { mapDBToModel } = require('../../utils');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
   constructor() {
@@ -24,5 +27,24 @@ class NotesService {
     }
 
     return result.rows[0].id;
+  }
+
+  async getNotes() {
+    const result = await this._pool.query('SELECT * FROM notes');
+    return result.rows.map(mapDBToModel);
+  }
+
+  async getNoteById(id) {
+    const query = {
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModel)[0];
   }
 }
